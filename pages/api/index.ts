3a1970323fd09cdf56297590a73a65679918636b
kjsik11 <kjsik11@gmail.com>
2021-09-03 22:08:47 +0900

    add options lib

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,15 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import puppeteer from 'puppeteer'
 import { withErrorHandler } from '@utils/with-error-handler';
+import { getOptions } from '@lib/options';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     const {darkMode} = req.query
-    const browserFetcher = puppeteer.createBrowserFetcher();
 
-    const revisionInfo = await browserFetcher.download('818858.');
-
-    const browser = await puppeteer.launch({ executablePath: revisionInfo.executablePath })
+    const options = await getOptions(process.env.NODE_ENV==='development');
+    
+    const browser = await puppeteer.launch(options)
 
     const page = await browser.newPage();
     //set size
