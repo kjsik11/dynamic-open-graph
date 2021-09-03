8e3014c19a3558c27563895682832344102944a6
kjsik11 <kjsik11@gmail.com>
2021-09-03 21:42:16 +0900

    make dynamic open graph page

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,9 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import puppeteer from 'puppeteer'
 import { withErrorHandler } from '@utils/with-error-handler';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    return res.json({ status: 'ok' });
+    const {darkMode} = req.query
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    //set size
+    await page.setViewport({width:2048,height:1170})
+    await page.goto(`http://localhost:3000?darkMode=${darkMode}`);
+
+    await page.waitForSelector('#image');          // wait for the selector to load
+    const element = await page.$('#image');        // declare a variable with an ElementHandle
+    if(!element) {
+      console.log('error')
+      return res.status(404).end()
+    }
+    const file = await element.screenshot({ type:'png' });
+  
+    await browser.close();
+
+    return res.send(file);
   }
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,104 +2,55 @@
  * @template PageComponent
  */
 
-import { useState } from 'react';
-
+import { useEffect,  useState } from 'react';
+import NextLink from 'next/link'
 import { Button } from '@components/ui';
+import { GetServerSideProps } from 'next';
 
-import { fetcher } from '@lib/fetcher';
-import { useModal } from '@lib/hooks/use-modal';
-import { useNoti } from '@lib/hooks/use-noti';
 
-export default function IndexPage() {
-  const { showModal, closeModal } = useModal();
-  const { showNoti, showAlert } = useNoti();
-  const [result, setResult] = useState<string | null>(null);
+interface Props {
+darkMode:'true'|'false'
+}
+
+export default function IndexPage({darkMode:darkModeQuery}:Props) {
+
+	const [darkMode,setDarkMode] = useState((darkModeQuery==='true'))
+
 
   return (
-    <div className="mx-auto max-w-screen-lg text-2xl pt-4 h-[1200px] flex justify-center">
-      {/* <p className="text-xl">hello world</p> */}
-      <div className="space-y-4 w-full">
-        <div
-          className="w-20 h-20 bg-black sm:bg-red-500 md:bg-yellow-500 lg:bg-blue-500 xl:bg-green-500 2xl:bg-blue-500"
-          aria-hidden="true"
-        />
-        <div
-          className="w-20 h-20 bg-black/40 sm:bg-red-500/40 md:bg-yellow-500/40 lg:bg-blue-500/40 xl:bg-green-500/40 2xl:bg-blue-500/40"
-          aria-hidden="true"
-        />
-        <div className="w-20 h-20 bg-gradient-to-br from-black/40 to-white/60" aria-hidden="true" />
-        <div>
-          <Button
-            color="red"
-            onClick={() =>
-              showModal({
-                variant: 'alert',
-                title: '댓글을 완전히 삭제할까요?',
-                content:
-                  '삭제된 댓글은 복구할 수 없습니다. 댓글에 달린 모든 답글도 함께 삭제됩니다.',
-                actionButton: {
-                  label: 'Delete',
-                  onClick: closeModal,
-                },
-              })
-            }
-          >
-            open alert modal
-          </Button>
-        </div>
-
-        <div>
-          <Button
-            onClick={() =>
-              showModal({
-                title: '댓글을 완전히 복구?',
-                content:
-                  '복구된 댓글은 삭제할 수 없습니다. 댓글에 달린 모든 답글도 함께 복구됩니다.',
-                actionButton: {
-                  label: 'Restore',
-                  onClick: closeModal,
-                },
-              })
-            }
-          >
-            open modal
-          </Button>
-        </div>
-        <div>
-          <Button
-            color="white"
-            onClick={() => showNoti({ title: '알람입니다.', variant: 'alert' })}
-          >
-            alert Noti
-          </Button>
-        </div>
-        <div className="space-x-4">
-          <Button
-            onClick={() => {
-              fetcher.get('/api').text().then(setResult).catch(showAlert);
-            }}
-          >
-            STATUS
-          </Button>
-          <Button
-            color="red"
-            onClick={() => {
-              fetcher.get('/api/error').text().then(setResult).catch(showAlert);
-            }}
-          >
-            ERROR
-          </Button>
-          <Button
-            color="white"
-            onClick={() => {
-              setResult(null);
-            }}
-          >
-            CLEAR
-          </Button>
-        </div>
-        <p className="text-lg">{result || 'null'}</p>
-      </div>
-    </div>
+	  <div className='mx-auto max-w-screen-lg pt-4'>
+		<div className="flex justify-center space-x-20">
+			<div>
+				<Button onClick={()=>setDarkMode((prev)=>!prev)}>{darkMode?'lightMode':'Darkmode'}</Button>
+			</div>
+			<div id='image' className='w-40 h-40 cursor-pointer border border-gray-400' >
+				{/*eslint-disable-next-line */}
+				<a href={`/api?darkMode=${darkMode}`} download='test.png'>
+						<svg version="1.1" style={{backgroundColor:darkMode?'black':'white'}} fill={darkMode?'white':'black'} id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						viewBox="0 0 512.003 512.003" xmlSpace="preserve">
+							<g>
+								<path d="M351.98,0c-27.296,1.888-59.2,19.36-77.792,42.112c-16.96,20.64-30.912,51.296-25.472,81.088
+								c29.824,0.928,60.64-16.96,78.496-40.096C343.916,61.568,356.556,31.104,351.98,0z"/>
+							</g>
+							<g>
+							<path d="M459.852,171.776c-26.208-32.864-63.04-51.936-97.824-51.936c-45.92,0-65.344,21.984-97.248,21.984
+							c-32.896,0-57.888-21.92-97.6-21.92c-39.008,0-80.544,23.84-106.88,64.608c-37.024,57.408-30.688,165.344,29.312,257.28
+							c21.472,32.896,50.144,69.888,87.648,70.208c33.376,0.32,42.784-21.408,88-21.632c45.216-0.256,53.792,21.92,87.104,21.568
+							c37.536-0.288,67.776-41.28,89.248-74.176c15.392-23.584,21.12-35.456,33.056-62.08
+							C387.852,342.624,373.932,219.168,459.852,171.776z"/>
+							</g>
+						</svg>
+					</a>
+				</div>
+			</div>
+		</div>
   );
 }
+
+
+export  const getServerSideProps:GetServerSideProps = async(context) => {
+const query = context.query
+	return {
+	  props: {...query}, // will be passed to the page component as props
+	}
+  }
